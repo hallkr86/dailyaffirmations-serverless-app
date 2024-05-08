fc27ade4200a813c29b6f4c86de51befcb9f414a
Kesha Hall <k_renee10@yahoo.com>
2024-05-08 11:01:04 -0400

    built and designed a functional form

---
+++ serverless.js
@@ -0,0 +1,47 @@
+var API_ENDPOINT = 'https://bkiqf73mel.execute-api.us-east-1.amazonaws.com/prod/dailyAffirmations';
+
+
+var errorDiv = document.getElementById('error-message')
+var successDiv = document.getElementById('success-message')
+var resultsDiv = document.getElementById('results-message')
+
+// function output returns input button contents
+function waitSecondsValue() { return document.getElementById('waitTime').value }
+function messageValue() { return document.getElementById('message').value }
+function emailValue() { return document.getElementById('email').value }
+
+function clearNotifications() {
+    errorDiv.textContent = '';
+    resultsDiv.textContent = '';
+    successDiv.textContent = '';
+}
+
+// When buttons are clicked, this is run passing values to API Gateway call
+document.getElementById('emailButton').addEventListener('click', function(e) { sendData(e, 'email'); });
+
+function sendData (e, pref) {
+    e.preventDefault()
+    clearNotifications()
+    fetch(API_ENDPOINT, {
+        headers:{
+            "Content-type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            waitTime: waitTimeValue(),
+            message: messageValue(),
+            email: emailValue()
+        }),
+        mode: 'cors'
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data)
+        successDiv.textContent = 'Submitted. But check the result below!';
+        resultsDiv.textContent = JSON.stringify(data);
+    })
+    .catch(function(err) {
+        errorDiv.textContent = 'Oops! Error Error:\n' + err.toString();
+        console.log(err)
+    });
+};
